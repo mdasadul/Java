1bbb1e990206e061f32aa7f23593502eeb5c5f0c
asad <mdasadul@ualberta.ca>
2016-02-04 22:28:42 -0500

    adding printing statement

--- TripleSum/src/main/java/com/rabib/TripleSum/TripleSum.java
+++ TripleSum/src/main/java/com/rabib/TripleSum/TripleSum.java
@@ -12,18 +12,19 @@ import java.io.InputStreamReader;
  */
 public class TripleSum 
 {
-	public int count(int[] numbers) {
+	public static int count(int[] numbers) {
 		int total = 0,N=numbers.length;
 		for(int i =0;i<N;i++) {
 			for(int j=0; j<N; ++j) {
 				for(int k=0;k<N;++k)
 				{
 					if(numbers[i]+numbers[j]+numbers[k] ==0) {
-						total+=1;
+						total +=1;
 					}
 				}
 			}
 		}
+		System.out.println(total);
 		return total;
 	}
 	public static void main(String[] args) throws IOException, NullPointerException {
@@ -39,7 +40,7 @@ public class TripleSum
 				number[counter] = Integer.parseInt(s);
 				System.out.println(s);
 			}	
-		
+		System.out.println(count(number));
 
 	}
 }
