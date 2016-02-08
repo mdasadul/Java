9bea9aa4080fe1443277e32ef61f94304344c9f3
asad <mdasadul@ualberta.ca>
2016-02-08 07:22:13 -0500

    adding new project about stack and queue

---
+++ StackQueue/src/main/java/com/rabib/StackQueue/StackQueue.java
@@ -0,0 +1,41 @@
+package com.rabib.StackQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class StackQueue 
+{
+	private class Node{
+		String item;
+		Node next;
+	}
+	public void push(String item) {
+		
+	}
+    public static void main( String[] args ) throws NumberFormatException, IOException
+    {
+    	InputStream inputStream = StackQueue.class.getResourceAsStream("/8ints.txt");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		int N = Integer.parseInt(reader.readLine());
+		int[] number = new int[N];
+		int counter =0;
+			String inputNumbers = reader.readLine();
+			for(String s:inputNumbers.split(" "))
+			{
+				number[counter] = Integer.parseInt(s);
+				System.out.println(number[counter]);
+				counter++;
+			}	
+		Arrays.sort(number);
+		
+	}
+    
+}
