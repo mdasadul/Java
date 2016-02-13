4060e9a96324f00f992140c5a5f931ccfc361ff5
asad <mdasadul@ualberta.ca>
2016-02-12 23:08:33 -0500

    adding BearPair

--- src/main/java/Topcoder/BearPair.java
+++ src/main/java/Topcoder/BearPair.java
@@ -3,13 +3,38 @@ package Topcoder;
 public class BearPair {
 
 	public int bigDistance(String s) {
-		int distance =0;
+		int distance = -1,  start = 0, end = s.length() - 1;
 		
+		while (start < end) {
+			if (s.charAt(start) == s.charAt(end)) {
+				start++;
+			}else {
+				distance = end-start;
+				break;
+			}
+		}
+		start = 0; end = s.length() - 1;
+		while (start < end) {
+			if (s.charAt(start) == s.charAt(end)) {
+				end--;
+			}else {
+				if(distance<(end-start)) {
+				distance = end-start;
+				}
+				break;
+			}
+		}
+
 		return distance;
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BearPair obj = new BearPair();
+		System.out.println(obj.bigDistance("xxyyxyxyyyyyyxxxyxyxxxyxyxyyyyyxxxxxxyyyyyyyyxxxxx"));
+		System.out.println(obj.bigDistance("bear"));
 
 	}
 
 }
+// abbbbbdcaa
