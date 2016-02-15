eee52d3364d864903ac70e3844ad146d00849fe0
asad <mdasadul@ualberta.ca>
2016-02-15 16:41:42 -0500

    adding Randomize Array

---
+++ src/main/java/Topcoder/RandomizeArray.java
@@ -0,0 +1,31 @@
+package Topcoder;
+
+import java.util.Random;
+
+public class RandomizeArray {
+
+	
+	public int[] randomize(int[] array) {
+		Random rnd = new Random();
+		for (int i = array.length - 1; i > 0; i--) {
+			int index = rnd.nextInt(i);
+			//System.out.println(index);
+			int temp = array[i];
+			array[i] = array[index];
+			array[index]= temp;
+
+		}
+		return array;
+	}
+	private void printArray(int[] arr) {
+		for(int i=0;i<arr.length;++i) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	public static void main(String[] args) {
+		int[] array= {2,1,3,4,5,6};
+		RandomizeArray obj = new RandomizeArray();
+		obj.printArray(obj.randomize(array));
+		
+	}
+}
